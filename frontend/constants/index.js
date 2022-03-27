494fd61cdab0886f35ecb2f03e75d320876e4e8c
[Kushagra Sarathe] <[kushagrasarathe@gmail.com]>
2022-03-27 17:21:04 +0530

    changed contract

--- frontend/constants/index.js
+++ frontend/constants/index.js
@@ -1,4 +1,6 @@
-export const CONTRACT_ADDRESS ="0xBF6e1F4c6d02926557E376918bAEA8BfaAA383c0";
+export const CONTRACT_ADDRESS = "0xaace313BDE24d5E806c2fefC3cE4017cfDa4628A";
+
+// verified "0xBF6e1F4c6d02926557E376918bAEA8BfaAA383c0";
 
 //  "0x55bc84CFc56A0ED10266Cf204302aE315C67b0bd";
 export const CONTRACT_ABI = [
@@ -114,6 +116,120 @@ export const CONTRACT_ABI = [
 		"type": "function"
 	}
 ];
+
+// verified[
+// 	{
+// 		"inputs": [],
+// 		"name": "acceptPayment",
+// 		"outputs": [],
+// 		"stateMutability": "payable",
+// 		"type": "function"
+// 	},
+// 	{
+// 		"inputs": [
+// 			{
+// 				"internalType": "address payable",
+// 				"name": "empAddress",
+// 				"type": "address"
+// 			},
+// 			{
+// 				"internalType": "enum Salary.Position",
+// 				"name": "_position",
+// 				"type": "uint8"
+// 			}
+// 		],
+// 		"name": "addEmployee",
+// 		"outputs": [],
+// 		"stateMutability": "nonpayable",
+// 		"type": "function"
+// 	},
+// 	{
+// 		"inputs": [],
+// 		"name": "payEmployees",
+// 		"outputs": [],
+// 		"stateMutability": "payable",
+// 		"type": "function"
+// 	},
+// 	{
+// 		"inputs": [],
+// 		"stateMutability": "nonpayable",
+// 		"type": "constructor"
+// 	},
+// 	{
+// 		"inputs": [
+// 			{
+// 				"internalType": "uint256",
+// 				"name": "",
+// 				"type": "uint256"
+// 			}
+// 		],
+// 		"name": "allEmployees",
+// 		"outputs": [
+// 			{
+// 				"internalType": "address",
+// 				"name": "",
+// 				"type": "address"
+// 			}
+// 		],
+// 		"stateMutability": "view",
+// 		"type": "function"
+// 	},
+// 	{
+// 		"inputs": [],
+// 		"name": "getAllEmployees",
+// 		"outputs": [
+// 			{
+// 				"internalType": "address[]",
+// 				"name": "",
+// 				"type": "address[]"
+// 			},
+// 			{
+// 				"internalType": "address[]",
+// 				"name": "",
+// 				"type": "address[]"
+// 			},
+// 			{
+// 				"internalType": "address[]",
+// 				"name": "",
+// 				"type": "address[]"
+// 			}
+// 		],
+// 		"stateMutability": "view",
+// 		"type": "function"
+// 	},
+// 	{
+// 		"inputs": [],
+// 		"name": "getContractBalance",
+// 		"outputs": [
+// 			{
+// 				"internalType": "uint256",
+// 				"name": "",
+// 				"type": "uint256"
+// 			}
+// 		],
+// 		"stateMutability": "view",
+// 		"type": "function"
+// 	},
+// 	{
+// 		"inputs": [
+// 			{
+// 				"internalType": "address",
+// 				"name": "",
+// 				"type": "address"
+// 			}
+// 		],
+// 		"name": "isEmployee",
+// 		"outputs": [
+// 			{
+// 				"internalType": "bool",
+// 				"name": "",
+// 				"type": "bool"
+// 			}
+// 		],
+// 		"stateMutability": "view",
+// 		"type": "function"
+// 	}
+// ];
 
 // [
 // 	{
