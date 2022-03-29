b524feb903be6a63114c8b9e96eef8820e1cdffc
[Kushagra Sarathe] <[kushagrasarathe@gmail.com]>
2022-03-29 22:53:23 +0530

    image optimized

--- frontend/components/WelcomeImage.js
+++ frontend/components/WelcomeImage.js
@@ -3,7 +3,7 @@ import Image from 'next/image';
 
 export default function WelcomeImage( props ) {
   return (
-    <Image src="/../cryptodevs/4.svg" alt="Picture of the author"
+    <Image src={require("../public/cryptodevs/4.svg")} alt="Welcome Picture"
       width={400}
       height={400} />
   )
--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,6 +1,18 @@
 /** @type {import('next').NextConfig} */
+
+const withPlugins = require('next-compose-plugins');
+const optimizedImages = require('next-optimized-images');
+
 const nextConfig = {
   reactStrictMode: true,
 }
 
 module.exports = nextConfig
+module.exports = withPlugins([
+  [optimizedImages, {
+    /* config for next-optimized-images */
+  }],
+
+  // your other plugins here
+
+]);
