c65f160f66d09d72d72ef188b531a9522d23515f
[Kushagra Sarathe] <[kushagrasarathe@gmail.com]>
2022-03-29 23:58:57 +0530

    reverted

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,10 +1,7 @@
 /** @type {import('next').NextConfig} */
 
-const optimizedImages = require('next-optimized-images');
-
 const nextConfig = {
   reactStrictMode: true,
 }
 
 module.exports = nextConfig;
-module.exports = optimizedImages();
